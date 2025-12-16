21daf10a48853b371c603121bfbe2a098682c42b
try-hard1010 <136584339+musamasalla@users.noreply.github.com>
2025-12-16 20:48:31 +0200

    Initial Commit

---
+++ 1099 Tax Guard - Quarterly Tax Estimator for Freelancers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  1099 Tax Guard - Quarterly Tax Estimator for Freelancers
+//
+//  Created by Musa Masalla on 2025/12/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
